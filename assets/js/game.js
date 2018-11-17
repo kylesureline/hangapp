acbfe3a02c24a43af956575b3848ab94e952d826
kylesureline <kscheuerlein@icloud.com>
2018-11-17 13:56:14 -0800

    Initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,421 @@
+function loadJSON(path, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				if (success)
+					success(JSON.parse(xhr.responseText));
+			} else {
+				if (error)
+					error(xhr);
+			}
+		}
+	};
+	xhr.open("GET", path, true);
+	xhr.send();
+}
+
+function openNav() {
+	document.getElementById("mySidebar").style.width = "250px";
+}
+function closeNav() {
+	document.getElementById("mySidebar").style.width = "0";
+}
+function toggleNav() {
+	var mySidebar = document.getElementById("mySidebar");
+	var myMain = document.getElementById("main");
+	if(mySidebar.style.width == "250px") {
+		closeNav();
+	} else {
+		openNav();
+	}
+}
+var hangman = {
+	wins: 0,
+	losses: 0,
+	guesses: 10,
+	difficulty: "Easy",
+	inProgress: false,
+	answer: "",
+	guessedWord: [],
+	guessedLetters: [],
+	def: "",
+	cacheDef: "",
+	getNumberOfGuesses: function() {
+		if( hangman.difficulty == "Easy") {
+			return 10;
+		} else if( hangman.difficulty == "Medium") {
+			return 7;
+		} else {
+			return 4;
+		}
+	},
+	toggleDifficultyCheck: function() {
+		// player has guessed something already
+		if(hangman.inProgress) {
+			var c = confirm("Changing difficulty in the middle of game will restart. Are you sure?");
+			if( c == true) {
+				hangman.toggleDifficulty();
+				hangman.beginGame();
+			}
+		}
+		// player hasn't yet guessed anything
+		// don't start new game
+		else {
+			hangman.toggleDifficulty();
+		}
+	},
+	toggleDifficulty: function() {
+		if(hangman.difficulty == "Easy") {
+			hangman.difficulty = "Medium";
+		} else if( hangman.difficulty == "Medium") {
+			hangman.difficulty = "Hard";
+		} else {
+			hangman.difficulty = "Easy";
+		}
+		hangman.guesses = hangman.getNumberOfGuesses();
+		document.getElementById("difficulty").innerHTML = hangman.difficulty;
+		setTimeout( closeNav, 250 );
+		hangman.drawFrame();
+	},
+	drawFrame: function() {
+		var frame = 10;
+		svgAnimator.draw(frame);
+		while(frame >= hangman.guesses) {
+			svgAnimator.draw(frame);
+			frame--;
+		}
+	},
+	wordBankWords: [],
+	wordBankDefs: [],
+	cacheWords: function() {
+		// cache 30 words at a time
+		if(hangman.wordBankWords.length < 50) {
+			var w = Word_List.getRandomWord();
+			hangman.getDef(w, true);
+			setTimeout( function() {
+				if(hangman.cacheDef != "") {
+					// found a word with a definition!
+					hangman.wordBankWords.push(w);
+					hangman.wordBankDefs.push(hangman.cacheDef);
+				}
+			}, 500);
+		}
+		hangman.saveWordBank();
+		document.getElementById("nav-cached-words").innerHTML = "Cached Words: " + hangman.wordBankWords.length;
+	},
+	saveWordBank: function () {
+		if (typeof(Storage) !== "undefined") {
+			localStorage.setItem("wordBankWords", JSON.stringify(hangman.wordBankWords));
+			localStorage.setItem("wordBankDefs", JSON.stringify(hangman.wordBankDefs));
+		} else {
+			console.log("Sorry, your browser does not support Web Storage...");
+		}
+	},
+	guessedWordStr: function() {
+		return hangman.guessedWord.toString().replace(/,/g, '');
+	},
+	chooseWord: function() {
+		if(hangman.wordBankWords == "") {
+			hangman.answer = Word_List.getRandomWord();
+			for( var x = 0; x < hangman.answer.length; x++ ) {
+				hangman.guessedWord[x] = " ";
+			}
+		} else {
+			hangman.answer = hangman.wordBankWords[0];
+			hangman.def = hangman.wordBankDefs[0];
+			hangman.wordBankWords.splice(0, 1);
+			hangman.wordBankDefs.splice(0, 1);
+			hangman.saveWordBank();
+		}
+	},
+	placeBlanks: function() {
+		document.getElementById("word").innerHTML = "";
+		for( var i = 0; i < hangman.answer.length; i++ ) {
+			var node = document.createElement("SPAN");
+			node.setAttribute("id", "answer-" + i);
+			var textNode = document.createTextNode("_");
+			node.appendChild(textNode);
+			if(hangman.answer[i] == " ") {
+				var b = document.createElement("BR");
+				document.getElementById("word").appendChild(b);
+			} else {
+				document.getElementById("word").appendChild(node);
+			}
+		}
+	},
+	getDef: function(word, toCache) {
+		var online = navigator.onLine;
+		if(online) {
+			var apiURL = 'https://dictionaryapi.com/api/v3/references/collegiate/json/' + word + '?key=cb690753-1eb8-4661-a7f4-9adf25057760';
+			var _json;
+			loadJSON(apiURL, function(data) {
+				_json = data;
+				// got a definition! :)
+				try {
+					var type = _json[0].fl;
+					var def = _json[0].shortdef[0];
+					if (def.length > 300) {
+						def = def.substring(0, 300);
+						def += "...";
+					}
+					if(toCache) {
+						hangman.cacheDef = type + ": " + def;
+					} else {
+						hangman.def = type + ": " + def;
+					}
+				}
+				// couldn't get a definition! :(
+				catch(err) {
+					if(toCache) {
+						hangman.cacheDef = "";
+					} else {
+						hangman.def = "";
+					}
+				}
+			}, function(xhr) {
+				hangman.def = "Error";
+			});
+		} else {
+			hangman.cacheDef = "";
+		}
+	},
+	isGuessedLetter: function(ltr) {
+		for( var u = 0; u < hangman.guessedLetters.length; u++ ) {
+			if( ltr == hangman.guessedLetters[u] ) {
+				return true;
+			}
+		}
+		return false;
+	},
+	guess: function() {
+		hangman.inProgress = true;
+		var ltr = this.innerHTML.toLowerCase();
+		var wrongGuess = 0;
+		
+		if( !hangman.isGuessedLetter(ltr) ) {
+			hangman.guessedLetters.push(ltr);
+			for(var t = 0; t < hangman.answer.length; t++ ) {
+				if( ltr === hangman.answer[t] ) {
+					document.getElementById("answer-" + t).innerHTML = ltr;
+					hangman.guessedWord[t] = ltr;
+				} else {
+					wrongGuess++;
+				}
+				document.getElementById("guess-" + ltr).style.display = "none";
+			}
+			if( wrongGuess == hangman.answer.length ) {
+				hangman.guesses--;
+				svgAnimator.draw(hangman.guesses);
+			}
+			if( hangman.answer == hangman.guessedWordStr() && hangman.guesses > 0) {
+				hangman.win();
+			} else if(hangman.guesses == 0) {
+				hangman.lose();
+			}
+		}
+	},
+	win: function() {
+		document.getElementById("win-lose").innerHTML = "You win! The word was <a href='https://www.merriam-webster.com/dictionary/" + hangman.answer + "/'>" + hangman.answer + "</a>!";
+		hangman.wins++;
+		hangman.endGame();
+	},
+	lose: function() {
+		document.getElementById("win-lose").innerHTML = "You lose! The word was <a href='https://www.merriam-webster.com/dictionary/" + hangman.answer + "/'>" + hangman.answer + "</a>!";
+		hangman.losses++;
+		hangman.endGame();
+	},
+	endGame: function() {
+		hangman.inProgress = false;
+		if (typeof(Storage) !== "undefined") {
+			localStorage.setItem("wins", hangman.wins);
+			localStorage.setItem("losses", hangman.losses);
+		} else {
+			console.log("Sorry, your browser does not support Web Storage...");
+		}
+		document.getElementById("definition").innerHTML = hangman.def;
+		hangman.printScore();
+		fadeOutEffect("game-screen");
+		fadeInEffect("end-screen");
+		document.getElementById("end-screen").style.display = "inline";
+		document.getElementById("new-game").children[0].disabled = false;
+	},
+	printScore: function() {
+		document.getElementById("score-wins").innerHTML = "Wins: " + hangman.wins;
+		document.getElementById("score-losses").innerHTML = "Losses: " + hangman.losses;	
+		document.getElementById("nav-wins").innerHTML = "Wins: " + hangman.wins;
+		document.getElementById("nav-losses").innerHTML = "Losses: " + hangman.losses;
+	},
+	showGuessedLetters: function() {
+		var guessedLetterChildren = document.getElementById("guessed-letters").children;
+		for( var i = 0; i < guessedLetterChildren.length; i++ ) {
+			guessedLetterChildren[i].style.display = "inline";
+		}
+	},
+	beginGame: function() {
+		closeNav();
+		fadeOutEffect("end-screen");
+		fadeInEffect("game-screen");
+		hangman.def = "";
+		hangman.printScore();
+		hangman.chooseWord();
+		hangman.getDef(hangman.answer, false);
+		hangman.guesses = hangman.getNumberOfGuesses();
+		hangman.drawFrame();
+		hangman.placeBlanks();
+		hangman.showGuessedLetters();
+		hangman.guessedWord = [];
+		hangman.guessedLetters = [];
+		document.getElementById("new-game").children[0].disabled = true;
+	},
+	init: function() {
+		if (typeof(Storage) !== "undefined") {
+			// day/night mode
+			if(localStorage.getItem("style") !== null) {
+				var s = localStorage.getItem("style");
+				setStyle(s);
+			} else {
+				setStyle("day"); // default theme
+			}
+			// wins/losses
+			if(localStorage.getItem("wins") !== null) {
+				var w = localStorage.getItem("wins");
+				hangman.wins = Number(w);
+			} else {
+				hangman.wins = 0;
+			}
+			if(localStorage.getItem("losses") !== null) {
+				var l = localStorage.getItem("losses");
+				hangman.losses = Number(l);
+			} else {
+				hangman.losses = 0;
+			}
+			if(localStorage.getItem("wordBankWords") !== null && localStorage.getItem("wordBankDefs") !== null) {
+				var c = localStorage.getItem("wordBankWords");
+				var d = localStorage.getItem("wordBankDefs");
+				hangman.wordBankWords = JSON.parse(c);
+				hangman.wordBankDefs = JSON.parse(d);
+			}
+		} else {
+			console.log("Sorry, your browser does not support Web Storage...");
+		}
+		hangman.attachEventHandlers();
+		hangman.beginGame();
+	},
+	attachEventHandlers: function() {
+		var body = document.getElementById("main");
+		var except = document.getElementById("openbtn");
+
+		body.addEventListener("click", function () {
+			closeNav();
+			}, false);
+		except.addEventListener("click", function (ev) {
+			toggleNav();
+			ev.stopPropagation();
+		}, false);
+		
+		var guessedLetters = document.getElementById("guessed-letters");
+		
+		for(var i = 0; i < guessedLetters.childElementCount; i++) {
+			var btn = guessedLetters.children[i];
+			var ltr = (btn.innerHTML).toLowerCase();
+			btn.setAttribute("id", "guess-" + ltr);
+			btn.onclick = hangman.guess;
+		}
+	},
+};
+
+var svgAnimator = {
+	draw: function(x) {
+		if( x == 10 ) {
+			for( var i = 0; i < 10; i++ ) {
+				var path = document.querySelector('#svg_' + i);
+				var length = path.getTotalLength();
+				// Clear any previous transition
+				path.style.transition = path.style.WebkitTransition =
+				  'none';
+				// Set up the starting positions
+				path.style.strokeDasharray = length + ' ' + length;
+				path.style.strokeDashoffset = '0';
+				// Trigger a layout so styles are calculated & the browser
+				// picks up the starting position before animating
+				path.getBoundingClientRect();
+				// Define our transition
+				path.style.transition = path.style.WebkitTransition =
+				  'stroke-dashoffset 2s ease-in-out';
+				// Go!
+				path.style.strokeDashoffset = length;
+			}
+		} else {
+			var path = document.querySelector('#svg_' + x);
+			var length = path.getTotalLength();
+			// Clear any previous transition
+			path.style.transition = path.style.WebkitTransition =
+			  'none';
+			// Set up the starting positions
+			path.style.strokeDasharray = length + ' ' + length;
+			path.style.strokeDashoffset = length;
+			// Trigger a layout so styles are calculated & the browser
+			// picks up the starting position before animating
+			path.getBoundingClientRect();
+			// Define our transition
+			path.style.transition = path.style.WebkitTransition =
+			  'stroke-dashoffset 2s ease-in-out';
+			// Go!
+			path.style.strokeDashoffset = '0';
+		}
+	}
+};
+
+function isLetter(str) {
+	return str.length === 1 && str.match(/[a-z]/i);
+}
+
+function setStyle(s) {
+	document.getElementById("day-night-stylesheet").setAttribute("href", "assets/css/" + s + ".css");
+	if(typeof(Storage) !== "undefined") {
+		localStorage.setItem("style", s);
+	} else {
+		console.log("Sorry, your browser does not support Web Storage...");
+	}
+}
+
+function fadeOutEffect(target) {
+	document.getElementById(target).style.opacity = 0;
+	document.getElementById(target).style.display = "none";
+	/*
+	var fadeTarget = document.getElementById(target);
+	var counter = 1;
+	fadeTarget.style.opacity = counter;
+	var fadeEffect = setInterval(function () {
+		if (counter > 0.1) {
+			counter -= 0.1
+			fadeTarget.style.opacity = counter.toFixed(1);
+		} else {
+			clearInterval(fadeEffect);
+			document.getElementById(target).style.display = "none";
+		}
+	}, 50);
+	*/
+}
+
+function fadeInEffect(target) {
+	document.getElementById(target).style.display = "inline";
+	var fadeTarget = document.getElementById(target);
+	var counter = 0;
+	fadeTarget.style.opacity = counter;
+	var fadeEffect = setInterval(function () {
+		if (counter < 0.9) {
+			counter += 0.1
+			fadeTarget.style.opacity = counter.toFixed(1);
+		} else {
+			clearInterval(fadeEffect);
+		}
+	}, 50);
+}
+
+// start caching words
+setInterval( hangman.cacheWords, 1000);
+
+
+window.onload = hangman.init;
---
+++ readme.md
@@ -0,0 +1,122 @@
+# Hangapp
+
+A simple Hangman game, built as a Progressive Web App. Playable [here](https://kylesureline.github.io). Also featured on [Appsco.pe](https://appsco.pe/app/hangapp)!
+
+## Features
+
+- Extensive word bank, curtesy of https://github.com/TimTCrouch/WordList-JS.
+- Wins/Losses stored in localStorage
+- Day/Night mode color schemes
+- Can be added to the home screen for offline use (service workers to cache necessary files)
+- Definitions are retrieved via API from Merriam-Webster.com
+- While online, some definitions will be cached for offline use, else no definition will be provided
+
+### Changelog
+
+**Version 1.14.1**
+
+- Added: score, app version, and number of cached words in the nav menu
+- Change: Increase localStorage limit to 50 words and definitions
+- Change: The word on the game over screen is now a link to the word's definition on Merriam Webster
+
+**Version 1.12**
+
+- Less delay between attempts to retrieve definitions in the background
+- Fix: actually use the definitions that have been cached in localStorage
+- Change: Increase localStorage limit to 30 words and definitions
+
+**Version 1.11**
+
+- Only attempt to retrieve definitions from api if online
+
+**Version 1.10**
+
+- Make #main slightly less than 100vh to prevent scroll bar from showing on Safari
+
+**Version 1.09**
+
+- Fix changing difficulty on end-screen
+
+**Version 1.08**
+
+- Fix animation for difficulty and new games
+
+**Version 1.07**
+
+- Truncate word definitions if they're too long
+- Improve reliability of caching definitions
+- Introduced a difficulty setting
+
+**Version 1.06**
+
+- Cache words and definitions in the background
+- Resize buttons slightly
+- Improve fadeOutEffect() performance
+
+**Version 1.05**
+
+- Change back to Merriam Webster's API
+
+**Version 1.04**
+
+- Use localStorage instead of cookies
+- Use Oxford API instead
+
+**Version 1.03**
+
+- Use Merriam Webster's API to include the definition of the word
+
+**Version 1.02**
+
+- Resize font on buttons for mobile
+
+**Version 1.01**
+
+- Use flexbox for the button sizes
+
+**Version 1**
+
+- Change navigation menu to the left side of the screen
+
+**Version 0.9.8.8**
+
+- Add a navigation menu
+
+**Version 0.9.8.7**
+
+- Use buttons to take input instead of device keyboard
+- Split view in landscape orientation
+
+**Version 0.9.4**
+
+- Change look of the guess box
+
+**Version 0.9.3**
+
+- Revert to previous service worker (removing old caches wasn't working correctly)
+
+**Version 0.9.2**
+
+- Add day/night css to cache
+
+**Version 0.9.1**
+
+- Resize width of end screen
+
+**Version 0.9**
+
+- Fade out/in between screens
+- Remove old caches using service worker
+
+**Version 0.8.9**
+
+- Blur input when game ends
+
+**Version 0.8.8**
+
+- Add end screen
+
+**Version 0.8.7**
+
+- Changed the size of things for mobile
+- Put wins/losses on two separate lines
