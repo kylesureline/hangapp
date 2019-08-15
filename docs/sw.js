bdf024535a2f68ae06c86fbab9c9fca1153c4b51
Kyle Scheuerlein <kyle@kylesureline.com>
2019-08-14 21:59:48 -0700

    include old version in repo

---
+++ docs/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'v1.51';
+
+var urlsToCache = [
+	'index.html',
+	'assets/css/style.css',
+	'assets/js/game.js',
+	'assets/js/wordBank.min.js',
+	'assets/js/wordList.min.js'
+];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			})
+			.then(function() {
+				self.skipWaiting();
+			})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			cache.match(event.request, {ignoreSearch: true});
+		})
+		.then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
