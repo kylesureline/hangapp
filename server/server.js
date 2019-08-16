37b87ddadbd2116a8c0459a23fcabe1160af9a04
Kyle Scheuerlein <kyle@kylesureline.com>
2019-08-16 13:48:11 -0700

    remove unused wordlist dependency

--- server/server.js
+++ server/server.js
@@ -4,10 +4,13 @@ const app = new express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
+console.log(process.env);
+
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
+  // res.redirect(`https://${req.headers.host}${req.url}`);
 });
 
 app.listen(port, () => {
