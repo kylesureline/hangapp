defff6f65997edbf52f06c7653925500658a37c8
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-15 14:41:45 -0800

    fix className errors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export const Header = () => (
   <header>
 		<h1>Hangapp</h1>
-		<button class="openbtn"><i class="fas fa-bars"></i> Settings</button>
+		<button className="openbtn"><i className="fas fa-bars"></i> Settings</button>
 	</header>
 );
