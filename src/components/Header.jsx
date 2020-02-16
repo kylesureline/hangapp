417ac5c5f803d6412a61701f337e8bff558b25c7
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-15 20:51:05 -0800

    remove header component

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Header = () => (
-  <header>
-		<h1>Hangapp</h1>
-		<button className="openbtn"><i className="fas fa-bars"></i> Settings</button>
-	</header>
-);
