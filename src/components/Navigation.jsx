58224a5b77c4af4a9f8fa7d0ca9522006912d62e
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-15 21:08:48 -0800

    add icons for navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { ReactComponent as PlaySVG } from '../images/play_arrow.svg';
+import { ReactComponent as SettingsSVG } from '../images/settings1.svg';
+import { ReactComponent as StatsSVG } from '../images/stars.svg';
+import { ReactComponent as DatabaseSVG } from '../images/storage.svg';
+import { ReactComponent as AboutSVG } from '../images/person.svg';
 
 export const Navigation = () => (
-  <nav>
-    <ul>
-      <li><PlaySVG /></li>
-      <li>Settings</li>
-      <li>Stats</li>
-      <li>Database</li>
-      <li>About</li>
+  <nav className="main-nav">
+    <ul className="nav-list">
+      <li className="nav-list__item">
+        <PlaySVG title="Play" className="icon icon--nav" />
+      </li>
+      <li className="nav-list__item">
+        <SettingsSVG title="Settings" className="icon icon--nav" />
+      </li>
+      <li className="nav-list__item">
+        <StatsSVG title="Stats" className="icon icon--nav" />
+      </li>
+      <li>
+        <DatabaseSVG title="Database" className="icon icon--nav" />
+      </li>
+      <li>
+        <AboutSVG title="About" className="icon icon--nav" />
+      </li>
     </ul>
   </nav>
 );
