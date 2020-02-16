98a97bffefe603aed103aaf0dec509380d5ef250
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-15 20:52:42 -0800

    add reducer and rename stats

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,10 +1,12 @@
 export const initialState = {
-  pastGames: [],
+  mode: 'word',
+  wordLength: 4,
+  lives: 10,
 };
 
 export const reducer = (state = initialState, action) => {
   switch(action.type) {
     default:
-      return state;
+      return state
   }
 };
--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 // reducers
-import { reducer as stats } from '../reducers/stats';
+import { reducer as pastGames } from '../reducers/pastGames';
+import { reducer as settings } from '../reducers/settings';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const configureStore = () => {
   const store = createStore(
     combineReducers({
-      stats
+      pastGames,
+      settings
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
