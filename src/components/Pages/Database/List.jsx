02b2b64f78a5e19f2f3fd5145ed6e69760f94cd5
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-16 22:30:24 -0800

    search the database

--- src/components/Pages/Database/List.jsx
+++ src/components/Pages/Database/List.jsx
@@ -4,13 +4,17 @@ import { useSelector } from 'react-redux';
 export const List = ({ search }) => {
   const db = useSelector(state => state.db);
   const { mode } = useSelector(state => state.settings);
-  const [results, setResults] = useState();
+  const slice = 25;
+  const filter = useCallback(value => {
+    return value.toLowerCase().includes(search.toLowerCase());
+  }, [search]);
 
   return (
     <ul className="database-list">
-      {db[`${mode}s`].map((item, index) => (
-        <li className="database-list__item">{item}</li>
+      {db[`${mode}s`].filter(filter).slice(0, slice).map((item, index) => (
+        <li key={index} className="database-list__item">{item}</li>
       ))}
+      <li className="database-list__item database-list__item--first50">(Showing up to the first {slice} results)</li>
     </ul>
   );
 };
