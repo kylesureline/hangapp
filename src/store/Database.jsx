c7bfa45df8a3231b6c7087b3c4a62b0090f64355
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-17 22:38:55 -0800

    cache definitions on an interval

--- src/store/Database.jsx
+++ src/store/Database.jsx
@@ -2,11 +2,16 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import wordBank from '../db/wordBank';
 import { ADD_WORDS, DONE_COMPILING } from '../reducers/actions';
+import { fetchData, isOnline, getCache, addToCache } from '../utils';
 
 export const Database = ({ children }) => {
   const { mode } = useSelector(state => state.settings);
+  const { doneCompiling, words } = useSelector(state => state.db);
+  const CACHE_MAX = 500;
+
   const dispatch = useDispatch();
 
+  // turns separate chunks into one big array
   useEffect(() => {
     if(mode === 'word') {
       wordBank.forEach((chunk, index)=> {
@@ -19,6 +24,42 @@ export const Database = ({ children }) => {
       });
     }
   }, [mode, dispatch]);
+
+  // fetches definitions and stores them in localStorage
+  useEffect(() => {
+    let isUnmounted = false;
+    let interval;
+
+    if(doneCompiling) {
+      interval = setInterval(() => {
+        // don't try to fetch if offline
+        if(isOnline() && getCache().length < CACHE_MAX) {
+          const word = words[Math.floor(Math.random() * words.length)];
+          fetchData(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=cb690753-1eb8-4661-a7f4-9adf25057760`)
+            .then(data => {
+              if(!isUnmounted) {
+                if(!data.every(e => typeof e === 'string')) {
+                  // some words have multiple meanings
+                  // choose one at random
+                  const { fl: wordType, shortdef: defs } = data[Math.floor(Math.random() * data.length)];
+                  const def = defs[Math.floor(Math.random() * defs.length)];
+
+                  // console.log(word, wordType, def);
+                  if(getCache().length < CACHE_MAX) {
+                    addToCache({word, wordType, def});
+                  }
+                }
+              }
+            })
+        }
+      }, 250);
+    }
+
+    return () => {
+      isUnmounted = true;
+      clearInterval(interval);
+    }
+  }, [doneCompiling]);
 
   return children;
 }
---
+++ src/utils/utils.js
@@ -0,0 +1,23 @@
+export const fetchData = (url) => {
+  const checkStatus = (response) => {
+    if(response.ok) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(new Error(response.statusText));
+    }
+  }
+
+  return fetch(url)
+            .then(checkStatus)
+            .then(res => res.json())
+            .catch(error => console.log('Looks like there was a problem', error));
+};
+
+export const isOnline = () => navigator.onLine;
+
+export const getCache = () => JSON.parse(localStorage.getItem('db')) || [];
+
+export const addToCache = word => {
+  const cache = getCache();
+  localStorage.setItem('db', JSON.stringify([...cache, word]));
+};
