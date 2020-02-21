91137b7a986d3c2d521e05fe081045b62fe5dafe
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-20 22:09:45 -0800

    fix warning

--- src/hooks/useSelectedWords.js
+++ src/hooks/useSelectedWords.js
@@ -5,7 +5,9 @@ import { formatWordObj } from '../utils';
 export const useSelectedWords = (search = '') => {
   const { mode, words } = useSelector(state => state.settings);
   const db = useSelector(state => state.db);
-  const filter = useCallback(({ word }) => word.join(' ').toLowerCase().includes(search.toLowerCase()) && word.join(' ').length >= words.minLength, [search]);
+  const filter = useCallback(({ word }) => (
+    word.join(' ').toLowerCase().includes(search.toLowerCase()) && word.join(' ').length >= words.minLength
+  ), [search, words.minLength]);
 
   const selectedWords = useCallback(() => {
     if(mode === 'words') {
