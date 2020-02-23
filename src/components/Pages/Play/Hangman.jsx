018fdc7b36dce1ee31c9d5cab95e1e63559161df
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-22 22:11:34 -0800

    ignore warning

--- src/components/Pages/Play/Hangman.jsx
+++ src/components/Pages/Play/Hangman.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useCallback } from 'react';
 import { ReactComponent as HangmanSVG } from '../../../images/hangman.svg';
 import { useSelector } from 'react-redux';
