1577a2b466d5c4a23b66426ba5d2aaf86f8b9850
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-23 23:18:40 -0800

    fix showing the hint when game is over

--- src/components/Pages/Play/Hint.jsx
+++ src/components/Pages/Play/Hint.jsx
@@ -6,8 +6,8 @@ export const Hint = ({ mode, wordsSettings, answer, isOver }) => {
     const { def, wordType } = answer;
     return (
       <h2 className="answer__hint">
-        {showDefinition || isOver && <span className="answer__hint__definition">{def}</span>}
-        {showWordType || isOver && <span className="answer__hint__word-type">{wordType}</span>}
+        {(showDefinition || isOver) && <span className="answer__hint__definition">{def}</span>}
+        {(showWordType || isOver) && <span className="answer__hint__word-type">{wordType}</span>}
       </h2>
     )
   } else {
