cbf20b8799569b896c21636238e5f1d6aadadbd4
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-23 20:40:59 -0800

    fix className

--- src/components/reusable/ToggleSwitch.jsx
+++ src/components/reusable/ToggleSwitch.jsx
@@ -3,6 +3,6 @@ import React from 'react';
 export const ToggleSwitch = ({ checked, onChange }) => (
   <div className="switch">
     <input type="checkbox" checked={checked} onChange={onChange} />
-    <span class="switch__slider" />
+    <span className="switch__slider" />
   </div>
 );
