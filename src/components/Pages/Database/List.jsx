acca516cb97a292f81830f1af3b6fb1590ca6135
Kyle Scheuerlein <kyle@kylesureline.com>
2020-02-27 23:40:57 -0800

    change list styles

--- src/components/Pages/Database/List.jsx
+++ src/components/Pages/Database/List.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useSelectedWords } from '../../../hooks/useSelectedWords';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { END_GAME } from '../../../reducers/actions';
 
 export const List = ({ search, isOver }) => {
   const { selectedWords } = useSelectedWords(search);
+  const { mode } = useSelector(state => state.settings);
   const dispatch = useDispatch();
 
   const slice = 25;
@@ -19,10 +20,16 @@ export const List = ({ search, isOver }) => {
       {selectedWords.slice(0, slice).map(({ words }, index) => {
         return (
           <li key={index} className="database-list__item">
-            <a
-              href={`https://www.merriam-webster.com/dictionary/${words}/`}
-              alt={`${words}'s Definition on m-w.com`}
-              >{words}</a>
+            {mode === 'words' ? (
+              <a
+                href={`https://www.merriam-webster.com/dictionary/${words}/`}
+                alt={`${words}'s Definition on m-w.com`}
+                target="_blank"
+                rel="noopener noreferrer"
+                >{words}</a>
+            ) : (
+              words
+            )}
           </li>
         );
       })}
--- src/components/Pages/Stats/PastGamesItem.jsx
+++ src/components/Pages/Stats/PastGamesItem.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { toRelevantTimeString } from '../../../utils';
+import { ReactComponent as WonSVG } from '../../../images/check_circle.svg';
+import { ReactComponent as LostSVG } from '../../../images/cancel.svg';
 
 export const PastGamesItem = ({ game }) => {
   const { answer, timestamp, won } = game;
 
   return (
     <li className="past-games__item">
-      <span>{answer.words.join(' ')}</span>
-      <span>{won ? 'won' : 'lost'}</span>
-      <span>{toRelevantTimeString(timestamp)}</span>
+      {won ? (
+        <WonSVG className="icon icon--won" />
+      ) : (
+        <LostSVG className="icon icon--lost" />
+      )}
+      <span className="past-games__item__text">{answer.words.join(' ')}</span>
+      <span className="past-games__item__timestamp">{toRelevantTimeString(timestamp)}</span>
     </li>
   );
 };
