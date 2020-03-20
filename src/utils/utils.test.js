b6169003f2290b944148765b08d363543ddc528e
Kyle Scheuerlein <kyle@kylesureline.com>
2020-03-20 07:33:18 -0700

    add tests for formatWordObj()

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -3,7 +3,7 @@ import {
   // isOnline,
   // getFromLS,
   // saveToLS,
-  // formatWordObj,
+  formatWordObj,
   numberWithCommas
   // savePastGame,
   // saveCurrentGame,
@@ -13,6 +13,32 @@ import {
 } from "./utils";
 
 describe("custom utility functions", () => {
+  describe("formatWordObj()", () => {
+    it("formats correctly without a definition", () => {
+      const word = "hockey";
+      const wordType = "";
+      const def = "";
+      const wordObj = formatWordObj(word);
+
+      expect(wordObj).toEqual({
+        words: [word],
+        wordType,
+        def
+      });
+    });
+    it("formats correctly with a definition", () => {
+      const word = "hockey";
+      const wordType = "noun";
+      const def = `a game played on an ice rink in which two teams of six players on skates use curved sticks to try to shoot a puck into the opponent's goal`;
+      const wordObj = formatWordObj(word, wordType, def);
+
+      expect(wordObj).toEqual({
+        words: [word],
+        wordType,
+        def
+      });
+    });
+  });
   describe("numberWithCommas()", () => {
     it("works with 1-3 digits", () => {
       expect(numberWithCommas(1)).toEqual("1");
