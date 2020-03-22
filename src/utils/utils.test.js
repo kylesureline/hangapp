eafac98565360283affac332f5f3f4e6aa2bf3be
Kyle Scheuerlein <kyle@kylesureline.com>
2020-03-22 14:30:51 -0700

    add tests for toRelevantTimeString()

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -7,8 +7,8 @@ import {
   // savePastGame,
   // saveCurrentGame,
   // getCurrentGame,
-  toHoursMinutesSeconds
-  // toRelevantTimeString
+  toHoursMinutesSeconds,
+  toRelevantTimeString
 } from "./utils";
 
 describe("custom utility functions", () => {
@@ -66,6 +66,27 @@ describe("custom utility functions", () => {
       const ms = 50 * 1000 + 2 * 60 * 1000 + 3 * 60 * 60 * 1000;
 
       expect(toHoursMinutesSeconds(ms)).toEqual("3h 2m ago");
+    });
+  });
+  describe("toRelevantTimeString()", () => {
+    it("returns 2m 50s for recent timestamp", () => {
+      const ms = 50 * 1000 + 2 * 60 * 1000;
+
+      expect(toRelevantTimeString(Date.now() - ms)).toEqual("2m 50s ago");
+    });
+    it("returns date for old timestamp", () => {
+      const ms = 2 * 24 * 60 * 60 * 1000;
+
+      const d = new Date();
+      d.setDate(new Date().getDate() - 2);
+
+      expect(toRelevantTimeString(Date.now() - ms)).toEqual(
+        d.toLocaleString("default", {
+          day: "numeric",
+          month: "numeric",
+          year: "numeric"
+        })
+      );
     });
   });
 });
